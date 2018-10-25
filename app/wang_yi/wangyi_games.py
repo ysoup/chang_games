d52fb74b614cb02ad692eef3057a5f9ea0ed975f
杨卫东 <yangwd@aibicoin.com>
2018-10-25 22:37:55 +0800

    增加下单系统操作界面

--- app/wang_yi/wangyi_games.py
+++ app/wang_yi/wangyi_games.py
@@ -2,7 +2,7 @@
 from flask import request, render_template, jsonify, flash, abort, url_for, redirect, session, Flask, g, current_app
 from . import wangyi
 from chang_games import redis_store
-import json
+import json, time, datetime
 
 
 @wangyi.route('/monitor_list', methods=['GET'])
@@ -26,55 +26,57 @@ def add_monitor_info():
         return render_template('wangyi_games/add_monitor_info.html')
     elif request.method == "POST":
         try:
-            title = request.form.get("title")
-            if title == None or title == "":
-                title = ""
-            info = NewFlashInformation(content=request.form['content'], category=request.form['category'],highlight_color=request.form['highlight_color'],
-                                       is_show=int(request.form['is_show']),is_push=int(request.form["is_push"]),
-                                       remarks=request.form['remarks'], source_name="ren_gong", content_id=int(time.time()), title=title)
-            db.session.add(info)
-            if info.is_push == 1:
-                content = title
-                content_text = copy.deepcopy(content)
-                content_text = filter_char(content_text)
-                code = push_service.common_push_server(content_text)
-                if code == 201:
-                    info.is_push = 1
-                    info.is_show = 1
-            db.session.commit()
+            dic = {}
+            dic["id"] = int(time.time())
+            dic["goods_name"] = request.form.get("goods_name")
+            dic["goods_url"] = request.form.get("goods_url")
+            dic["safe_code"] = request.form.get("safe_code")
+            dic["user_cookie"] = request.form.get("title")
+            dic["remarks"] = request.form.get("title")
+            dic["create_time"] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            redis_store.set("new_monitor_data_%s" % dic["id"], json.dumps(dic))
+
+            cache_data = redis_store.get("monitor_list")
+            ls = []
+            if cache_data:
+                ls = json.loads(str(cache_data, encoding="utf-8"))
+                ls.append(dic)
+            else:
+                ls.append(dic)
+            redis_store.set("monitor_list", json.dumps(ls))
             return jsonify({'success': 'ok'})
-        except Exception as e :
+        except Exception as e:
             current_app.logger.error(e)
-            db.session.rollback()
             return jsonify({'failed': 'ok'})
 
 
-# @wangyi_games.route('/create_monitor_data', methods=['GET', 'POST'])
-# def create_monitor_data():
-#     if request.method == "GET":
-#         return render_template('wangyi_games/create_monitor_data.html')
-#     elif request.method == "POST":
-#         try:
-#             title = request.form.get("title")
-#             if title == None or title == "":
-#                 title = ""
-#             info = NewFlashInformation(content=request.form['content'], category=request.form['category'],highlight_color=request.form['highlight_color'],
-#                                        is_show=int(request.form['is_show']),is_push=int(request.form["is_push"]),
-#                                        remarks=request.form['remarks'], source_name="ren_gong", content_id=int(time.time()), title=title)
-#             db.session.add(info)
-#             if info.is_push == 1:
-#                 content = title
-#                 content_text = copy.deepcopy(content)
-#                 content_text = filter_char(content_text)
-#                 code = push_service.common_push_server(content_text)
-#                 if code == 201:
-#                     info.is_push = 1
-#                     info.is_show = 1
-#             db.session.commit()
-#             return jsonify({'success': 'ok'})
-#         except Exception as e :
-#             current_app.logger.error(e)
-#             db.session.rollback()
-#             return jsonify({'failed': 'ok'})
+# 编辑监控商品
+@wangyi.route("/modify_monitor_data", methods=['GET', 'POST'])
+def modify_monitor_data():
+    if request.method == "GET":
+        id = request.args.get('id', type=int)
+        data = redis_store.get("new_monitor_data_%s" % id)
+        return render_template('wangyi_games/modify_monitor_data.html', data=json.loads(str(data, encoding="utf-8")))
+    elif request.method == "POST":
+        try:
+            id = request.form.get('id')
+            goods_name = request.form.get('goods_name')
+            goods_url = request.form.get('goods_url')
+            safe_code = request.form.get('safe_code')
+            user_cookie = request.form.get('user_cookie')
+            remarks = request.form.get('remarks')
+            data = redis_store.get("new_monitor_data_%s" % id)
+            data = json.loads(str(data, encoding="utf-8"))
+            data["goods_name"] = goods_name
+            data["goods_url"] = goods_url
+            data["safe_code"] = safe_code
+            data["user_cookie"] = user_cookie
+            data["remarks"] = remarks
+            redis_store.set("new_monitor_data_%s" % id, json.dumps(data))
+            return jsonify({"success": "ok"})
+        except Exception as e:
+            current_app.logger.error(e)
+            return jsonify({'failed': '修改失败'})
+
 
 
